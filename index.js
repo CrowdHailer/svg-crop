bc497132cac3a6c8942effc4da9c5929dfbde1b5
Peter <peterhsaxton@gmail.com>
2014-08-22 14:49:46 +0100

    lint code

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
+'use strict';
+
 var readSVG = require('./lib/readDimensions');
 var BBox = require('bbox');
 var interpolate = require('interpolate');
 var fs = require('fs');
 var shell = require('shelljs');
 
-var STRING_TEMPLATE = 'inkscape --export-area={x0}:{y0}:{x1}:{y1} --export-png=www/{output} --export-height={height} {raw}'; 
+var STRING_TEMPLATE = 'inkscape --export-area={x0}:{y0}:{x1}:{y1} --export-png=www/{output} --export-height={height} {raw}';
 
-module.exports = function(array, originalPath){
-    var raw = fs.readFileSync(originalPath)
-    readSVG(raw, function(container){
+module.exports = function (array, originalPath) {
+    var raw = fs.readFileSync(originalPath);
+    readSVG(raw, function (container) {
         array.forEach(function (copyDimensions) {
-            var cutOut = BBox.fit(container, BBox.create(copyDimensions.width, copyDimensions.height));
-            var str = interpolate(STRING_TEMPLATE, {
-                x0: cutOut.x0,
-                y0: cutOut.y0,
-                x1: cutOut.x1,
-                y1: cutOut.y1,
-                output: copyDimensions.path,
-                height: copyDimensions.height,
-                raw: originalPath,
-            })
+            var cutOut = BBox.fit(container, BBox.create(copyDimensions.width, copyDimensions.height)),
+                str = interpolate(STRING_TEMPLATE, {
+                    x0: cutOut.x0,
+                    y0: cutOut.y0,
+                    x1: cutOut.x1,
+                    y1: cutOut.y1,
+                    output: copyDimensions.path,
+                    height: copyDimensions.height,
+                    raw: originalPath,
+                });
             shell.exec(str);
-        })
-    })
-}
+        });
+    });
+};
--- test.js
+++ test.js
@@ -12,7 +12,7 @@ beforeEach(function () {
     readSVG = require('./lib/readDimensions');
     crop = proxyquire('./index', {'shelljs': shellStub});
     BBox = require('bbox');
-    dummy = jasmine.createSpy()
+    dummy = jasmine.createSpy();
 });
 
 describe('Reading and SVGs dimensions', function () {
@@ -29,8 +29,8 @@ describe('Reading and SVGs dimensions', function () {
 
 describe('Sending commands', function () {
     it('should log each command that gets executed', function () {
-        crop([{path: 'a', width: 200, height: 100}, {path: 'b', width: 100, height: 100}], 'fixture/blank.svg')
-        expect(shellDummy).toHaveBeenCalledWith('inkscape --export-area=0:0:200:100 --export-png=www/a --export-height=100 fixture/blank.svg')
-        expect(shellDummy).toHaveBeenCalledWith('inkscape --export-area=50:0:150:100 --export-png=www/b --export-height=100 fixture/blank.svg')
+        crop([{path: 'a', width: 200, height: 100}, {path: 'b', width: 100, height: 100}], 'fixture/blank.svg');
+        expect(shellDummy).toHaveBeenCalledWith('inkscape --export-area=0:0:200:100 --export-png=www/a --export-height=100 fixture/blank.svg');
+        expect(shellDummy).toHaveBeenCalledWith('inkscape --export-area=50:0:150:100 --export-png=www/b --export-height=100 fixture/blank.svg');
     });
 });
